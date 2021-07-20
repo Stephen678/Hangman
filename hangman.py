f95c9c2234bd9dd641d310eb7792b0cffd4f553b
Chigbolu Somto <chigbolustephen@gmail.com>
2021-07-20 11:50:09 +0100

    Dictionary to store guesses

--- hangman.py
+++ hangman.py
@@ -12,11 +12,12 @@ def display_word(wording):
     guessed_letters = []
     guessed_words = []
     tries = 6
+    guesses = {}
     print("Let's play Hangman!")
     print(word_completion)
     print("\n")      
     while not guessed and tries > 0:
-        guess = input("Please guess a letter: ").upper() 
+        guess = input("Please guess a letter: ").upper()
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
                 print("You already guessed the letter", guess)
@@ -24,12 +25,14 @@ def display_word(wording):
                 print(guess, "is not in the word.")
                 tries -= 1
                 guessed_letters.append(guess)
+                guesses[guess] = False
             else:
                 print("YES", guess)
                 guessed_letters.append(guess)
+                guesses[guess] = True
                 wording_as_list = list(word_completion)  
-                indices = [i for i, letter in enumerate(wording) if letter == guess]
-                print(indices)    
+                indices = [i for i, letter in enumerate(wording) if letter == guess] 
+                print(indices) 
                 for index in indices:
                     wording_as_list[index] = guess
                 word_completion = "".join(wording_as_list)  
@@ -51,18 +54,21 @@ def display_word(wording):
         print(word_completion)
         print("\n")
     if guessed:
-        print("Congrats, you guessed the word! You win!")  
+        print("Congrats, you guessed the word! You win!")
+        print(guesses)  
     else:
         print("Sorry,you ran out of tries. The word was" + wording + ". Maybe next time!")          
         
-
-
+    
 def main():
     wording = get_word()
     display_word(wording)        
     while input("Play again? (Y/%)").upper() == "Y":
         wording = get_word()
-        display_word(wording)        
+        display_word(wording)
+
+    
+
 
         
 
